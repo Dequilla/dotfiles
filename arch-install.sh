49a94fb2b0e0c93159037c8883960f2180c36245
Edwin Wallin <e@edwinwallin.com>
2024-12-01 15:44:48 +0100

    Updated arch-install.sh to use Neovim instead.

--- arch-install.sh
+++ arch-install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+CONF_HOME=~/.config
+
 # Prerequesits
 sudo pacman -S --noconfirm bat the_silver_searcher ripgrep
 
 # Ensure vim is installed
-sudo pacman -S --noconfirm vim
+sudo pacman -S --noconfirm neovim
 
 # Setup folders
-mkdir -p $HOME/.vim/autoload $HOME/.vim/plugged $HOME/.vim/colors
-
-# Colorscheme
-curl -o $HOME/.vim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+mkdir -p $CONF_HOME/nvim/autoload $CONF_HOME/nvim/plugged $CONF_HOME/nvim/colors
 
 # Install plugin for handling plugins
-curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo $CONF_HOME/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Copy vimrc to correct location 
-cp ./vimrc $HOME/.vimrc
+cp ./nvim/init.vim $CONF_HOME/nvim/init.vim
+
+nvim +'PlugInstall'
